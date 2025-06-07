93819586ab1e288791da48026c6ab683a9ec85e0
prowebcoder <rahul@prowebcoder.com>
2025-06-07 18:29:35 +0530

    Latest Section added

--- components/otherPages/Breadcumb.jsx
+++ components/otherPages/Breadcumb.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Breadcumb() {
   return (
     <div
-      style={{ marginTop: "80px" }}
+      style={{ marginTop: "112px" }}
       className="breadcrumbs panel z-1 py-2 bg-secondary dark:bg-gray-100 dark:bg-opacity-5 dark:text-white"
     >
       <div className="container max-w-xl">
