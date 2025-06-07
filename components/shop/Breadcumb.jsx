f67fef3b86c9e70b81de62a98e3281c12ea60654
prowebcoder <rahul@prowebcoder.com>
2025-06-07 18:32:23 +0530

    Latest Section added

--- components/shop/Breadcumb.jsx
+++ components/shop/Breadcumb.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Breadcumb({ pageName }) {
   return (
     <div
-      style={{ marginTop: "80px" }}
+      style={{ marginTop: "112px" }}
       className="breadcrumbs panel z-1 py-2 bg-secondary dark:bg-gray-100 dark:bg-opacity-5 dark:text-white"
     >
       <div className="container max-w-xl">
--- components/shop/Breadcumb2.jsx
+++ components/shop/Breadcumb2.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Breadcumb2({ product }) {
   return (
     <div
-      style={{ marginTop: "80px" }}
+      style={{ marginTop: "112px" }}
       className="breadcrumbs panel z-1 py-2 bg-secondary dark:bg-gray-100 dark:bg-opacity-5 dark:text-white"
     >
       <div className="container max-w-xl">
