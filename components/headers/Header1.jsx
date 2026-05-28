3e305dcf236d1c51d346b09ab4c3f4eb1c6a1f89
Prowebcoder <rahul@prowebcoder.com>
2026-05-28 15:48:47 +0530

    sitemap

--- components/headers/Header1.jsx
+++ components/headers/Header1.jsx
@@ -31,6 +31,21 @@ export default function Header1() {
   }, [prevScrollPos]);
   return (
     <>
+    <div className="announcement-bar">
+  <div className="announcement-track">
+    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
+     &nbsp;&nbsp;&nbsp;&nbsp;
+    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
+     &nbsp;&nbsp;&nbsp;&nbsp;
+    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
+     &nbsp;&nbsp;&nbsp;&nbsp;
+    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
+     &nbsp;&nbsp;&nbsp;&nbsp;
+    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
+  </div>
+</div>
     <header
       className={`uc-header header-default uc-navbar-sticky-wrap z-999 uc-sticky ${
         scrollingUp ? " uc-sticky-below uc-sticky-fixed headerFixed" : ""
@@ -166,24 +181,7 @@ export default function Header1() {
           </div>
         </div>
       </nav>
-      <div className="announcement-bar">
-  <div className="announcement-track">
-    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
-    &nbsp;&nbsp;&nbsp;&nbsp;
-    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
-     &nbsp;&nbsp;&nbsp;&nbsp;
-    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
-     &nbsp;&nbsp;&nbsp;&nbsp;
-    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
-     &nbsp;&nbsp;&nbsp;&nbsp;
-    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
-     &nbsp;&nbsp;&nbsp;&nbsp;
-    ⚠️ Important Notice: We do NOT send emails to customers. If you receive any email claiming to be from us, it is spam. Please ignore and do not click any links.
-  </div>
-</div>
     </header>
-    {/* Announcement Marquee */}
-
 </>
   );
 }
